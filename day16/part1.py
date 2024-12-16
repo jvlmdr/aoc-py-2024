ac5b76462f590353c1f27451d032ffc6d6f95fcb
Jack Valmadre <jack.valmadre@gmail.com>
2024-12-16 19:06:35 +1030

    Remove heuristic

--- day16/part1.py
+++ day16/part1.py
@@ -26,33 +26,29 @@ def main():
     goal = tuple(map(int, np.squeeze(np.asarray(np.where(grid == 'E')))))
     is_free = grid != '#'
 
-    def heur(pos, dpos):
-        rel = tuple(map(lambda a, b: a - b, goal, pos))
-        return sum(map(abs, rel))
-
     pos, dpos = start, (0, 1)
-    q = [(heur(pos, dpos), 0, pos, dpos)]
-    min_cost_seen = {}
+    q = [(0, pos, dpos)]
+    visited = set()
     while q:
-        _, cost, pos, dpos = heapq.heappop(q)
+        cost, pos, dpos = heapq.heappop(q)
         if pos == goal:
             print(cost)
             return
 
-        if (pos, dpos) in min_cost_seen and min_cost_seen[pos, dpos] < cost:
+        if (pos, dpos) in visited:
             # Already visited this state with a lower cost. Do not extend.
             continue
-        min_cost_seen[pos, dpos] = cost
+        visited.add((pos, dpos))
 
         # Move forward.
         pos_ = tuple(map(lambda a, b: a + b, pos, dpos))
         if is_free[pos_]:
             cost_ = cost + 1
-            heapq.heappush(q, (cost_ + heur(pos_, dpos), cost_, pos_, dpos))
+            heapq.heappush(q, (cost_, pos_, dpos))
         # Turn on the spot.
         for dpos_ in TURNS[dpos]:
             cost_ = cost + 1000
-            heapq.heappush(q, (cost_ + heur(pos, dpos_), cost_, pos, dpos_))
+            heapq.heappush(q, (cost_, pos, dpos_))
 
     print('not reachable')
 
--- day16/part2.py
+++ day16/part2.py
@@ -25,17 +25,13 @@ def main():
     goal = tuple(map(int, np.squeeze(np.asarray(np.where(grid == 'E')))))
     is_free = grid != '#'
 
-    def heur(pos, dpos):
-        rel = tuple(map(lambda a, b: a - b, goal, pos))
-        return sum(map(abs, rel))
-
     pos, dpos = start, (0, 1)
-    q = [(heur(pos, dpos), 0, pos, dpos, frozenset([pos]))]
+    q = [(0, pos, dpos, frozenset([pos]))]
     min_cost_seen = {}
     best_cost = None
     any_path = None
     while q:
-        _, cost, pos, dpos, path = heapq.heappop(q)
+        cost, pos, dpos, path = heapq.heappop(q)
         if pos == goal:
             if best_cost is None or cost < best_cost:
                 best_cost = cost
@@ -53,11 +49,11 @@ def main():
         pos_ = tuple(map(lambda a, b: a + b, pos, dpos))
         if is_free[pos_]:
             cost_ = cost + 1
-            heapq.heappush(q, (cost_ + heur(pos_, dpos), cost_, pos_, dpos, path | frozenset([pos_])))
+            heapq.heappush(q, (cost_, pos_, dpos, path | frozenset([pos_])))
         # Turn on the spot.
         for dpos_ in TURNS[dpos]:
             cost_ = cost + 1000
-            heapq.heappush(q, (cost_ + heur(pos, dpos_), cost_, pos, dpos_, path))
+            heapq.heappush(q, (cost_, pos, dpos_, path))
 
     print('min cost:', best_cost)
     print('on path:', len(any_path))
